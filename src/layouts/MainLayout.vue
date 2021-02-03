bcf944093339266763f4a55f46638f331696920c
AndersonTeala <andersonteala@gmail.com>
2021-02-03 14:55:30 -0300

    Modified page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,44 +1,34 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
+      <div class="q-pa-md q-gutter-y-sm">
+
+    <div class=" text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-btn color="accent" icon="manage_accounts" label="Login" />
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space />
+
+        <q-btn flat round dense icon="shopping_cart" class="q-mr-xs" />
       </q-toolbar>
-    </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+      <q-toolbar inset>
+
+        <div class="q-mr-xs" style="margin-top: 10px">
+        <q-img src="../../public/images/logo.png" width="150px" />
+      </div>
+
+        <q-breadcrumbs active-color="accent" style="color: #8257e6" class="absolute-center text-h5">
+          <q-breadcrumbs-el label="Home" icon="home" />
+          <q-breadcrumbs-el label="Sobre" icon="fingerprint" />
+          <q-breadcrumbs-el label="Produtos" icon="book" />
+          <q-breadcrumbs-el label="Contato" icon="phone" />
+        </q-breadcrumbs>
+      </q-toolbar>
+    </div>
+  </div>
+    </q-header>
 
     <q-page-container>
       <router-view />
@@ -47,61 +37,18 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data () {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
-    }
+    return {}
   }
 }
 </script>
+import {
+  Quasar,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QImg
+} from 'quasar'
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,25 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
+
+    <div class="flex flex-center q-mt-md q-mr-sm">
+      <q-img src="https://cdn.culturagenial.com/imagens/death-note-cartaz.jpg" width="250px" />
+    </div>
+
+    <q-card
+      class="my-card text-white q-mt-md q-mr-sm"
+      style="background: radial-gradient(circle, #8257e6 0%, #8257e6 100%)"
     >
+      <q-card-section>
+        <div class="text-h3">Livro Death Note</div><br/>
+        <div class="text-subtitle2" style="font-size: 22px">R$ 32,90</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <p>A Macabra Filmes, em parceria com a DarkSide Books,<br/> realiza um grande sonho dos fãs de horror no Brasil: a<br/> coleção completa de Livros de Sangue, de Clive Barker,<br/> chega para os leitores brasileiros com um tratamento…<br/> visceral.</p>
+      </q-card-section>
+      <q-btn color="green" label="COMPRAR" class="relative-position" style="margin: 10px" />
+    </q-card>
+
   </q-page>
 </template>
 
@@ -12,3 +28,13 @@ export default {
   name: 'PageIndex'
 }
 </script>
+import {
+  Quasar,
+  QImg,
+  QBtn,
+  QToolbar,
+  QToolbarTitle,
+  QCard,
+  QCardSection,
+  QCardActions
+} from 'quasar'
